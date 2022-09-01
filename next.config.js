5495762a2f994d3b08a7bdbe8653bdfde7425dc4
art-of-michelangelo <omarx816@hotmail.com>
2022-09-01 17:50:55 +0300

    err fix

--- next.config.js
+++ next.config.js
@@ -3,21 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env:{
-    apiKey: "AIzaSyAxws6Q7gXSEojmErwfP2YAbG_tBPaNAIQ",
-    authDomain: "mywebsite-62e8c.firebaseapp.com",
-    databaseURL: "https://mywebsite-62e8c-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "mywebsite-62e8c",
-    storageBucket: "mywebsite-62e8c.appspot.com",
-    messagingSenderId: "284601990552",
-    appId: "1:284601990552:web:1d971d15c5c8fb3ea87fed",
-    measurementId: "G-JQNSPQ67M6",
-    MONGODB_URI: "mongodb+srv://omarx816:135790Omer@cluster0.dcjbm.mongodb.net/?retryWrites=true&w=majority",
-    GOOGLE_CLIENT_ID: "284601990552-66k2rvm6fgh667u8e6ol36gsvafsuvm1.apps.googleusercontent.com",
-    
-    GOOGLE_CLIENT_SECRET: "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",
-    AWS_ACCESS_KEY_ID1: "AKIATZVWWAVE33HSI6GC",
-    AWS_SECRET_ACCESS_KEY1: "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",
-    AWS_BUCKET_NAME: "myqrcodeapp",
+
   },
   
 }
