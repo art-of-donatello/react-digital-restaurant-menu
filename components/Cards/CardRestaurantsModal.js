387934166106175059625c27597e0f59a2083b7d
art-of-michelangelo <omarx816@hotmail.com>
2022-09-01 19:08:27 +0300

    fix

--- components/Cards/CardRestaurantsModal.js
+++ components/Cards/CardRestaurantsModal.js
@@ -25,8 +25,9 @@ export default function CardRestaurantsModal() {
   return (
         <>
         {showModal?<CardCreateMenu  showModal={showModal} setShowModal={setShowModal}  />:null}
-        <button onClick={()=>setShowModal(!showModal)} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Create Default Menu</button>
+        
           <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+          
             <div className="rounded-t mb-0 px-4 py-3 border-0">
               <div className="flex flex-wrap items-center">
                 <div className="relative w-full px-4 max-w-full flex-grow flex-1">
@@ -35,12 +36,8 @@ export default function CardRestaurantsModal() {
                   </h3>
                 </div>
                 <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
-                  <button
-                    className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    Menus
-                  </button>
+              
+                  <button onClick={()=>setShowModal(!showModal)}  className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">Create Default Menu</button>
                 </div>
               </div>
             </div>
